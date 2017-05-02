197707ca31dabc6905380be7ddd0a4dd5f6f3ab8
Michael DeJesus <mad@cs.tamu.edu>
2017-05-02 11:28:00 -0500

    Adding basic main python file to help as skeleton for most apps

---
+++ main_skeleton.py
@@ -0,0 +1,65 @@
+import sys
+import traceback
+import math
+import numpy
+import scipy.stats
+
+
+import pytransit.transit_tools as transit_tools
+import pytransit.norm_tools as norm_tools
+import pytransit.tnseq_tools as tnseq_tools
+
+
+def error(x):
+    print "Error: %s" % x
+
+def usage():
+    print """python %s -pt <Annotation .prot_table or .gff3>  -f <Comma seperated list of.wig files> [options]
+
+        Optional Arguments:
+            -n <string>     :=  Normalization method. Default: -n TTR
+            --help          :=  Prints this help message and quits.
+""" % sys.argv[0]
+
+
+
+
+def main(args, kwargs):
+
+    # Get arguments
+    norm_method = kwargs.get("n", "TTR")
+
+    # Data processing
+    wiglist = kwargs["f"].split(",")
+    (data, position) = tnseq_tools.get_data(wiglist)
+    (norm_data, factors) = norm_tools.normalize_data(data, method=norm_method)
+
+    
+
+
+if __name__ == "__main__":
+
+     # Helper function to parse inputs
+    (args, kwargs) = transit_tools.cleanargs(sys.argv[1:])
+
+    # If asking for help, print help and exit
+    if "-help" in kwargs:
+        usage()
+        sys.exit()
+
+    # Check required arguments
+    missingArgs = False
+    if "f" not in kwargs:
+        missingArgs = True
+        error("Missing -f argument")
+    if "pt" not in kwargs:
+        missingArgs = True
+        error("Missing -pt argument")
+
+    if missingArgs:
+        usage()
+        sys.exit()
+
+    main(args, kwargs)
+
+    
